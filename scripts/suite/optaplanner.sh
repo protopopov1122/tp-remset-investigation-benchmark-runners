25643ba738c8bb1c1b8be6845150803e6d1a263f
J. Protopopovs <jprotopopov1122@gmail.com>
2022-11-21 19:29:47 +0100

    Update optaplanner runner script

--- scripts/suite/optaplanner.sh
+++ scripts/suite/optaplanner.sh
@@ -9,21 +9,17 @@ GC_LOGS="$BENCHMARK_RESULT_DIR/gc_logs"
 
 mkdir "$GC_LOGS"
 mkdir "$BENCHMARK_TMPDIR/jdk"
-mkdir "$BENCHMARK_TMPDIR/downloads"
 mkdir "$BENCHMARK_TMPDIR/maven"
 
 ln -s "$JAVA_HOME" "$BENCHMARK_TMPDIR/jdk/benchmarked"
 
-cd "$BENCHMARK_TMPDIR/downloads"
-wget "https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz"
-
 cd "$BENCHMARK_TMPDIR"
-tar -xf downloads/apache-maven-3.8.6-bin.tar.gz -C ./maven/
+tar -xf "$BENCHMARK_SUITE_BASE_DIR/optaplanner/apache-maven-3.8.6-bin.tar.gz" -C ./maven/
 
-cp "$BENCHMARK_SUITE_BASE_DIR/optaplanner/optaplanner.sh" .
+cp "$BENCHMARK_SUITE_BASE_DIR/optaplanner/optaplanner_custom.sh" .
 export JAVA_OPTIONS="$JAVA_OPTIONS $(java_gc_log_flags $GC_LOGS/gc.log)"
 export _JAVA_OPTIONS="$_JAVA_OPTIONS $(java_gc_log_flags $GC_LOGS/gc.log)"
 export JAVA_OPTS="$JAVA_OPTS $(java_gc_log_flags $GC_LOGS/gc.log)"
-./optaplanner.sh
+./optaplanner_custom.sh "$BENCHMARK_SUITE_BASE_DIR/optaplanner"
 
 cp -r "$BENCHMARK_TMPDIR/results" "$BENCHMARK_RESULT_DIR"
