d67f613d6763fc56e040492e689ff622272790cc
J. Protopopovs <jprotopopov1122@gmail.com>
2022-12-19 20:52:00 +0100

    Implement optional dropping of Renaissance result outliers

--- analysis/loaders.py
+++ analysis/loaders.py
@@ -2,6 +2,8 @@
 from pathlib import Path
 from typing import List
 import pandas
+import os
+import math
 
 def q05(x):
     return x.quantile(0.05)
@@ -64,7 +66,17 @@ def load_pjbb2005_throughput(benchmark_runs: List[Path]) -> pandas.DataFrame:
 
 def load_renaissance(benchmark_runs: List[Path]) -> pandas.DataFrame:
     df = load_csv_frames(benchmark_runs, 'benchmarks/renaissance/results.csv')
-    df = df.groupby(['benchmark'])[['duration_ns']].agg(AGG_FN)
+    df = df[['benchmark', 'duration_ns']]
+    drop_outliers = float(os.getenv('RENAISSANCE_DROP_OUTLIERS', default='0'))
+    if drop_outliers > 0:
+        drop_outliers *= 0.5
+        grouping = df.groupby(['benchmark'], as_index=False, group_keys=False)
+        min_outliers = grouping.apply(lambda grp: grp.nsmallest(math.floor(drop_outliers * len(grp)), columns='duration_ns'))
+        df.drop(min_outliers.index, inplace=True)
+        grouping = df.groupby(['benchmark'], as_index=False, group_keys=False)
+        max_outliers = grouping.apply(lambda grp: grp.nlargest(math.floor(drop_outliers * len(grp) / (1.0 - drop_outliers)), columns='duration_ns'))
+        df.drop(max_outliers.index, inplace=True)
+    df = df.groupby(['benchmark']).agg(AGG_FN)
     df.columns = AGG_COLUMNS
     return df
 
